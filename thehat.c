9712bcc5a731ca2bd1dcc43add2d66b65933da98
Amanda de Paiva Filizola <amandapaivafilizola@gmail.com>
2016-07-18 02:39:20 -0300

    comentário do começo do questionário

--- thehat.c
+++ thehat.c
@@ -10,5 +10,8 @@ int main(void)
 {
     int x;
     printf("Voce esta comecando o questionario para determinar sua Casa. Responda o mais honestamente possivel, pois do contrario, recebera uma Casa na qual nao se encaixa./n");
+    /*comeco do questionario*/
+
+
 
 }
