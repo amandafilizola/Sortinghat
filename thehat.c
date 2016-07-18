ecf408c823e74a0c96476723000fb60ca11f4fd5
Amanda de Paiva Filizola <amandapaivafilizola@gmail.com>
2016-07-18 02:34:58 -0300

    Create thehat.c

---
+++ thehat.c
@@ -0,0 +1,14 @@
+/*
+Autora: Amanda de Paiva Filizola
+The Sorting Hat
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int x;
+    printf("Voce esta comecando o questionario para determinar sua Casa. Responda o mais honestamente possivel, pois do contrario, recebera uma Casa na qual nao se encaixa./n");
+
+}
