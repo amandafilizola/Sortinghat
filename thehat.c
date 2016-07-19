7f58f81d50e32d5bec30d9194c1bec932f8a85ae
Amanda de Paiva Filizola <amandapaivafilizola@gmail.com>
2016-07-19 18:23:33 -0300

    primeira pergunta

--- thehat.c
+++ thehat.c
@@ -9,9 +9,14 @@ The Sorting Hat
 int main(void)
 {
     int x;
-    printf("Voce esta comecando o questionario para determinar sua Casa. Responda o mais honestamente possivel, pois do contrario, recebera uma Casa na qual nao se encaixa./n");
+    printf("Voce esta comecando o questionario para determinar sua Casa. Responda o mais honestamente possivel, pois do contrario, recebera uma Casa na qual nao se encaixa./nDigite o numero da resposta escolhida e pressione <enter>.");
+
     /*comeco do questionario*/
-
-
+    /*pergunta 1 */
+    printf("Qual a caracteristica que voce mais preza:\n");
+    pritnf("1.Coragem.\n");
+    printf("2.Lealdade.\n");
+    printf("3.Inteligencia.\n");
+    printf("4.Astúcia.\n");
 
 }
