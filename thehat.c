7b834b205fa719d4a89aa3e5fa2aa6a0f822dadb
Amanda de Paiva Filizola <amandapaivafilizola@gmail.com>
2016-07-30 22:50:08 -0300

    resposta 15 ravenclaw

--- thehat.c
+++ thehat.c
@@ -253,6 +253,6 @@ int main(void)
     printf("Cinco anos atras um garoto roubou seu dinheiro e nunca teve confusao por causa disso. Desde entao, ele se tornou uma pessoa muito melhor. Recentement, ele foi preso por roubo, porem voce pode provar que ele e inocente.\n\n");
     printf("1.Nao digo nada. Essa e a punicao que ele nao teve cinco anos atras.\n");
     printf("2.Vou salva-lo pois sei que ele e uma pessoa melhor agora.\n");
-
+    printf("3.Eu o salvaria mesmo que ele fosse uma pessoa ma porque eu sei que ele e inocente dessa vez.\n");
                       
 }   
