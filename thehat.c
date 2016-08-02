aa40c1b51a9cdf372cfc84b73d8cbde50a33cba9
Amanda de Paiva Filizola <amandapaivafilizola@gmail.com>
2016-08-01 23:54:09 -0300

    resposta 17 ravenclar

--- thehat.c
+++ thehat.c
@@ -287,4 +287,5 @@ int main(void)
     printf("Voce se considera uma pessoa que toma riscos?\n");
     printf("1.Com certeza.\n");
     printf("2.Apenas se forem para ajudar alguem.\n");
+    printf("3.Apenas se for estrategicamente vantajoso.\n");
 }   
